52fd64d319478c3ad8ccedb02379e5554b2f407a
teknoKirtley <kkirtley@teknoluxion.com>
2019-02-22 11:49:50 -0600

    GCS-38048 Updated the nitf21Response.ts file to only return an array of trigraph strings for the country code return values as needed by EMC. updated cfg.env with lates io urls for services.

--- processors/nitf21Response.ts
+++ processors/nitf21Response.ts
@@ -5,10 +5,14 @@ export class Nitf21Response extends BaseProcessor {
 
         const result = new Promise<ProcessorResponse>(async (resolve, reject) => {
             const output: any = Object.assign({}, this.executionContext.raw);
+            const CountryOutput: Array<string> = [];
             output.GEO = {};
             output.GEO.WKT = this.executionContext.raw.wkt;
             output.GEO.GeoJSON = this.executionContext.raw.geoJson;
-            output.GEO.Countries = this.executionContext.raw.countries;
+            // Populate the CountryOutput array with just the GENC_3 values returned from CountryCode service
+            this.executionContext.raw.countries.forEach((x: { GENC_3: string; }) => CountryOutput.push(x.GENC_3));
+            output.GEO.CountryCodes = CountryOutput;
+
             resolve({
                 data: {GEO: output.GEO},
                 successful: true,
@@ -16,6 +20,5 @@ export class Nitf21Response extends BaseProcessor {
         });
 
         return result;
-
     }
 }
