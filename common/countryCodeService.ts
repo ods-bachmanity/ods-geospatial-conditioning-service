526182d77797d9be4f63d2da08fcbc5ba0db30b6
Michael Hartman <Michael.A.Hartman@outlook.com>
2019-03-04 13:26:11 -0600

    Updated the health check to query the health of the two dependent services (country code and coordinate conversion) to check whether they are reachable and return that status as part of health check.

--- common/countryCodeService.ts
+++ common/countryCodeService.ts
@@ -3,7 +3,9 @@ import { Logger } from './logger';
 
 export class CountryCodeService {
 
-    private servicePath: string = process.env.COUNTRYCODESERVICEURL;
+    private servicePath: string = process.env.COUNTRYCODESERVICE_BASEURL;
+    private healthEndpoint: string = process.env.COUNTRYCODESERVICE_HEALTHENDPOINT;
+    private countriesEndpoint: string = process.env.COUNTRYCODESERVICE_COUNTRIESENDPOINT;
     private logger: Logger = new Logger();
 
     public constructor(private correlationId: string) {}
@@ -15,7 +17,11 @@ export class CountryCodeService {
             try {
                 if (!this.servicePath) {
                     this.logger.error(this.correlationId, `Invalid Service Path for Country Code Service`, `COUNTRYCODESERVICE.GET`);
-                    return reject({message: `Invalid Service Path for Country Code Service`});
+                    return reject({message: `Invalid Service Path for Country Code Service, update environment value for COUNTRYCODESERVICE_BASEURL`});
+                }
+                if (!this.countriesEndpoint) {
+                    this.logger.error(this.correlationId, `Invalid countries endpoint for Country Code Service`, `COUNTRYCODESERVICE.GET`);
+                    return reject({message: `Invalid countries endpoint for Country Code Service, update environment value for COUNTRYCODESERVICE_COUNTRIESENDPOINT`});
                 }
 
                 if (!wktInput) {
@@ -28,7 +34,7 @@ export class CountryCodeService {
                         wkt: wktInput,
                     }),
                     headers: { 'content-type': 'application/json' },
-                    url: this.servicePath,
+                    url: this.servicePath + this.countriesEndpoint,
                 });
 
                 const records = JSON.parse(response);
@@ -39,6 +45,35 @@ export class CountryCodeService {
                 return reject(err);
             }
 
+        });
+
+        return result;
+    }
+
+    public getHealth(): Promise<any> {
+        const result: Promise<any> = new Promise(async (resolve, reject) => {
+            try {
+                if (!this.servicePath) {
+                    this.logger.error(this.correlationId, `Invalid Service Path for Country Code Service`, `COUNTRYCODESERVICE.GETHEALTH`);
+                    return reject({message: `Invalid Service Path for Country Code Service, update environment value for COUNTRYCODESERVICE_BASEURL`});
+                }
+                if (!this.healthEndpoint) {
+                    this.logger.error(this.correlationId, `Invalid health endpoint for Country Code Service`, `COUNTRYCODESERVICE.GETHEALTH`);
+                    return reject({message: `Invalid health endpoint for Country Code Service, update environment value for COUNTRYCODESERVICE_HEALTHENDPOINT`});
+                }
+
+                const response = await rp.get({
+                    url: this.servicePath + this.healthEndpoint,
+                });
+
+                const body = JSON.parse(response);
+                body.healthy = true;
+                return resolve(body);
+
+            } catch (err) {
+                this.logger.warn(this.correlationId, `Warning, cannot reach Country Code Service: ${err.message}`, `COUNTRYCODESERVICE.GETHEALTH`);
+                return resolve(err);
+            }
         });
 
         return result;
