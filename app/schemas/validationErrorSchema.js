72360d6da69dd52c9435ac23d9ca4d4274607dd7
teknoKirtley <kkirtley@teknoluxion.com>
2019-04-10 12:21:38 -0500

    Removed all js files from the app folder. removed duplicate .gitignore from the app folder. Modified .gitignore to not track js files within the app folder.

--- app/schemas/validationErrorSchema.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class ValidationErrorSchema {
-    constructor() {
-        this.errors = false;
-        this.errString = '';
-    }
-}
-exports.ValidationErrorSchema = ValidationErrorSchema;
